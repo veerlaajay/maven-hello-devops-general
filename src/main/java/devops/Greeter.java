30ce7c0906c876d175b662fe8dc3621df36028d3
teachmycloud <143493780+cloudatwork@users.noreply.github.com>
2023-12-27 10:23:22 +0530

    all commits

---
+++ src/main/java/devops/Greeter.java
@@ -0,0 +1,7 @@
+package devops;
+
+public class Greeter {
+	public String sayHello() {
+		return "Hello DevOps Engineers";
+	}
+}
---
+++ src/main/java/devops/HelloDevOps.java
@@ -0,0 +1,12 @@
+package devops;
+
+import org.joda.time.LocalTime;
+
+public class HelloDevOps {
+	public static void main(String[] args) {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+	}
+}
